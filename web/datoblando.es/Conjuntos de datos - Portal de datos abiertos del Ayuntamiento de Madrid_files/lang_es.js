d1bb606b6d74c9033751966cc7fa423a834f5e76
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2022-05-07 12:10:10 +0200

    remove js from homepage

--- web/datoblando.es/Conjuntos de datos - Portal de datos abiertos del Ayuntamiento de Madrid_files/lang_es.js
+++ web/datoblando.es/Conjuntos de datos - Portal de datos abiertos del Ayuntamiento de Madrid_files/lang_es.js
@@ -1,30 +1,36 @@
 //Literales estructurales
-arrLit["es"]=[];
-arrLit["es"]["volver"]=["volver"];
-arrLit["es"]["imprimir"]=["imprimir"];
-arrLit["es"]["mostrar"]="mostrar";
-arrLit["es"]["ocultar"]="ocultar";                          
-var literal=[];
-literal["error"]="Corrija el siguiente error:";
-literal["elCampo"]="El campo";
-literal["noEsCorrecto"]="no es correcto";
-literal["recuerdaQueEl"]="Recuerda que el";
-literal["limiteDeEdadEs"]="límite de edad es";
-literal["elFormatoCorrectoEs"]="el formato correcto es";
-literal["num8CifrasYletra"]="8cifras+letra sin ningún espacio";
-literal["campoObligatorio"]="Campo obligatorio";
-literal["debeTenerExactamente"]="deber tener exactamente";
-literal["suLongMaximaEsDe"]="su longitud máxima es de";
-literal["suLongMinimaEsDe"]="su longitud mínima es de";
-literal["debeSelecionarOpcion"]="Debes seleccionar una de las opciones";
-literal["esDemasiado"]="es demasiado";
-literal["corto"]="corto";
-literal["largo"]="largo";
-literal["anios"]="años";
-literal["caracter"]="caracter";
-literal["caracteres"]="caracteres";
-literal["codigoNoEspaniol"]="El dato introducido no es un código postal español";
-literal["debesIntroducirUn"]="Debes introducir un/a";
-literal["valido"]="válido/a";
-literal["portlets.comentarios.mostrarComentarios"]="Mostrar comentarios y valoraciones";
-literal["portlets.comentarios.ocultarComentarios"]="Ocultar comentarios y valoraciones";
+if (arrLit === undefined) {
+    var arrLit = [];
+}
+arrLit["es"] = [];
+arrLit["es"]["volver"] = ["volver"];
+arrLit["es"]["imprimir"] = ["imprimir"];
+arrLit["es"]["mostrar"] = "mostrar";
+arrLit["es"]["ocultar"] = "ocultar";
+var literal = [];
+literal["error"] = "Corrija el siguiente error:";
+literal["elCampo"] = "El campo";
+literal["noEsCorrecto"] = "no es correcto";
+literal["recuerdaQueEl"] = "Recuerda que el";
+literal["limiteDeEdadEs"] = "límite de edad es";
+literal["elFormatoCorrectoEs"] = "el formato correcto es";
+literal["num8CifrasYletra"] = "8cifras+letra sin ningún espacio";
+literal["campoObligatorio"] = "Campo obligatorio";
+literal["debeTenerExactamente"] = "deber tener exactamente";
+literal["suLongMaximaEsDe"] = "su longitud máxima es de";
+literal["suLongMinimaEsDe"] = "su longitud mínima es de";
+literal["debeSelecionarOpcion"] = "Debes seleccionar una de las opciones";
+literal["esDemasiado"] = "es demasiado";
+literal["corto"] = "corto";
+literal["largo"] = "largo";
+literal["anios"] = "años";
+literal["caracter"] = "caracter";
+literal["caracteres"] = "caracteres";
+literal["codigoNoEspaniol"] =
+    "El dato introducido no es un código postal español";
+literal["debesIntroducirUn"] = "Debes introducir un/a";
+literal["valido"] = "válido/a";
+literal["portlets.comentarios.mostrarComentarios"] =
+    "Mostrar comentarios y valoraciones";
+literal["portlets.comentarios.ocultarComentarios"] =
+    "Ocultar comentarios y valoraciones";
